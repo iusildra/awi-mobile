fdf34f53dfdc174c0ecb9dadab47a80f195d8944
Richard MARTIN <richard.martin@polydev.app>
2023-03-14 15:45:53 +0100

    rework ContentView.swift

--- awi/Pages/HomeView.swift
+++ awi/Pages/HomeView.swift
@@ -7,7 +7,13 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            Text("Home")
+            Spacer().frame(height: 20)
+            Text("Welcome to FestiGames");
+            Spacer().frame(height: 40)
+            Text("An app to help you find the games you want to play at FestiGames 2023")
+                    .padding(.horizontal, 20)
+                    .multilineTextAlignment(.allCases[1])
+
             Spacer()
         }
     }
